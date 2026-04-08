eb77ab1a795e5aedab1ad8ff63d60c1ff2111878
Mykyta Shchedrin <81822790+ennwy@users.noreply.github.com>
2026-04-08 09:20:43 +0300

    Time: 320 ms (5.52%) | Memory: 43.6 MB (12.91%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -2,13 +2,13 @@ class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         ps = sorted(zip(position, speed))[::-1]
         fleets = 0
-        prev_t = None
+        prev_t = float('-inf')
         print(ps)
 
         for p, s in ps:
             t = (target - p) / s
 
-            if not prev_t or t > prev_t:
+            if t > prev_t:
                 fleets += 1
                 prev_t = t
 
