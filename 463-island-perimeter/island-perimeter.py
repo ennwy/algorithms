55632cbc0030312c3b510356a56f5a6c8938ffc2
Mykyta Shchedrin <81822790+ennwy@users.noreply.github.com>
2026-06-11 10:41:49 +0300

    Time: 54 ms (55.18%) | Memory: 19.6 MB (51.71%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -6,15 +6,11 @@ class Solution:
             for x in range(len(grid[0])):
                 if grid[y][x] == 0:
                     continue
-                
-                if y == 0 or grid[y-1][x] == 0:
-                    res += 1
-                if x == 0 or grid[y][x-1] == 0:
-                    res += 1
-                if y == len(grid) - 1 or grid[y+1][x] == 0:
-                    res += 1
-                if x == len(grid[0]) - 1 or grid[y][x+1] == 0:
-                    res += 1
+    
+                res += y == 0 or grid[y-1][x] == 0
+                res += x == 0 or grid[y][x-1] == 0
+                res += y == len(grid) - 1 or grid[y+1][x] == 0
+                res += x == len(grid[0]) - 1 or grid[y][x+1] == 0
                 
         return res
                 
