a4139396421dbec7e913d56dea3ecf9235f75bb1
Mykyta Shchedrin <81822790+ennwy@users.noreply.github.com>
2026-06-15 20:37:24 +0300

    Time: 19 ms (25.71%) | Memory: 19.8 MB (97.95%) - LeetSync

--- 4271-find-the-degree-of-each-vertex/find-the-degree-of-each-vertex.py
+++ 4271-find-the-degree-of-each-vertex/find-the-degree-of-each-vertex.py
@@ -4,7 +4,7 @@ class Solution:
 
         for i in range(len(matrix) - 1): # i = 1
             for j in range(i+1, len(matrix)): # j = 2
-                    ans[i] += matrix[i][j]
-                    ans[j] += matrix[i][j]
+                ans[i] += matrix[i][j]
+                ans[j] += matrix[i][j]
 
         return ans
