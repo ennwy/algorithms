c73d31addf7546633f85b818e32073488f293768
Mykyta Shchedrin <81822790+ennwy@users.noreply.github.com>
2026-06-20 13:06:17 +0300

    Time: 0 ms (100.00%) | Memory: 20.2 MB (89.55%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,38 +1,37 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        preMap = { i:[] for i in range(numCourses)}
+        deg = [0] * numCourses
+        graph = [[] for _ in range(numCourses)]
+
         for course, pre in prerequisites:
-            preMap[course].append(pre)
+            graph[pre].append(course)
+            deg[course] += 1
 
         visited = set()
-        res = set()
+        q = deque()
         order = []
 
-        def dfs(course: int) -> bool:
-            if course in visited:
-                return False
-            if len(preMap[course]) == 0:
-                if course not in res:
-                    order.append(course)
-                    res.add(course)
-                return True
-
-            visited.add(course)
-
-            for pre in preMap[course]:
-                if not dfs(pre):
-                    return False
-            
-            if course not in res:
+        for course in range(len(deg)):
+            if deg[course] == 0:
+                visited.add(course)
+                q.append(course)
                 order.append(course)
-                res.add(course)
-            visited.remove(course)
-            preMap[course] = []
-            
-            return True
+
+
+        while q:
+            pre = q.popleft()
+            for course in graph[pre]:
+                if course in visited:
+                    return []
+
+                deg[course] -= 1
+                if deg[course] <= 0:
+                    visited.add(course)
+                    q.append(course)
+                    order.append(course)
         
-        for course in range(numCourses):
-            if not dfs(course):
-                return []
-            
-        return order
+        return order if len(visited) == numCourses else []
+
+                
+
+
