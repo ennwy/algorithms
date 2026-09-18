9e1a030a5814c130a834b8c2d862936d46a9dbbf
Mykyta Shchedrin <81822790+ennwy@users.noreply.github.com>
2026-09-18 11:32:43 +0300

    Time: 20 ms (48.84%) | Memory: 19.4 MB (89.46%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,23 +1,35 @@
 class Solution:
-    def maxAreaOfIsland(self, grid: List[List[str]]) -> int:
-        def area(r, c) -> int:
-            if r >= len(grid) or r < 0 or c >= len(grid[0]) or c < 0 or grid[r][c] == 0:
-                return 0
-            
+    def maxAreaOfIsland(self, grid: list[list[int]]) -> int:
+        def unvisited(r, c) -> bool:
+            return 0 <= r < len(grid) and 0 <= c < len(grid[0]) and grid[r][c] == 1
+
+        directions = ((1,0), (-1, 0), (0, -1), (0, 1))
+        
+        def add_neightbours(r, c, q):
+            for dr, dc in directions:
+                if unvisited(dr+r, dc+c):
+                    grid[dr+r][dc+c] = 0
+                    q.append((dr+r, dc+c))
+        
+        def bfs(r, c) -> int:
+            q = deque()
+            q.append((r,c))
             grid[r][c] = 0
 
-            return 1 + area(r + 1, c) + area(r - 1, c) + area(r, c + 1) + area(r, c - 1)
-        
-        maxa = 0
+            area = 0
 
+            while q:
+                cr, cc = q.popleft()
+                area += 1
+                
+                add_neightbours(cr, cc, q)
+            
+            return area
+
+        maxarea = 0
         for r in range(len(grid)):
             for c in range(len(grid[0])):
                 if grid[r][c] == 1:
-                    a = area(r, c)
-                    maxa = max(maxa, a)
-
-        return maxa
-                
-
-
-
+                    maxarea = max(maxarea, bfs(r, c))
+        
+        return maxarea
